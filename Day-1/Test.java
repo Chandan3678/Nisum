35001f6703ae92a6e4b80bc615159ee1a826321c
Chandan3678 <rathchandan88@gmail.com>
2025-05-31 17:57:06 +0530

    Test.java done

--- Day-1/Test.java
+++ Day-1/Test.java
@@ -19,3 +19,48 @@
         				obj.hello();   		 	
 			}
 		}
+i) Constant holder : the value is constant now can be used to store the values like PI.
+		final class HiYo {
+    			public static final String s = "Avonash";
+
+		}
+
+		public class TestConstants {
+    			public static void main(String[] args) {
+        				System.out.println(HiYo.s);
+    			}
+		}
+
+
+
+		  ii) Prevents Inheritance: Enforce strict control of logic. It cannot be extended which helps in improving security and stability.
+			final class NoExtend {
+    				public void show() {
+        					System.out.println("This class cannot be extended.");
+   				}
+			}
+		  iii) Prevents Overriding - 
+
+			class Parent {
+		    		public final void display() {
+				   System.out.println("This is a final method in Parent class.");
+    				}
+			}
+
+			class Child extends Parent {
+     			 /*
+    				public void display() {
+        					System.out.println("Trying to override in Child class.");
+    				}
+   			 */
+			} // if we comment out then we will get compile error
+
+			public class FinalMethodExample {
+    				public static void main(String[] args) {
+        					Child c = new Child();
+        					c.display();
+		}
+			}
+
+		
+		iv) Performance Benefit (JVM Optimization): JVM can inline static/final calls.
