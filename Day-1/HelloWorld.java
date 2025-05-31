2ae20edc89f90760da5489599ec84bee9ad0bec8
Chandan3678 <rathchandan88@gmail.com>
2025-05-31 18:11:28 +0530

    HelloWorld.java done

---
+++ Day-1/HelloWorld.java
@@ -0,0 +1,8 @@
+		// this file should be in package1 which is a folder basically
+		package package1;
+
+		public class HelloWorld {
+    			public void hello() {
+        				System.out.println("Hello from package1!");
+    			}
+		}
