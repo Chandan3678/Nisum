d5a0308435da94c675401dc112cd6a241dfedd97
Chandan3678 <rathchandan88@gmail.com>
2025-05-31 18:00:42 +0530

    RetChar.java done

--- Day-1/RetChar.java
+++ Day-1/RetChar.java
@@ -1,6 +1,6 @@
 		public class RetChar {
     			public static void main(String[] args) {
-        				String s = "Avinash Rath";
+        				String s = "Chandan Rath";
         				int index = 3;
         				char ch = s.charAt(index);
         				System.out.println("Character at index " + index + " is: " + ch);
