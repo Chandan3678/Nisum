e8f31a1005ce7b4c5f9341f62a893025eb5ffb49
Chandan3678 <rathchandan88@gmail.com>
2025-05-31 18:00:17 +0530

    RevStr.java done

---
+++ Day-1/RevStr.java
@@ -0,0 +1,8 @@
+			public class RevStr {
+    				public static void main(String[] args) {
+        					String s = "I am Chandan Rath";
+        					StringBuffer sb = new StringBuffer(s);
+        					sb.reverse();
+        					System.out.println("Reversed String: " + sb);
+    				}
+			}
