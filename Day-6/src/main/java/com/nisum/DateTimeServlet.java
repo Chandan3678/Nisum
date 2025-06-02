54dd1da9ba6dd774c3cad4fc38059a6c82786707
Chandan3678 <rathchandan88@gmail.com>
2025-06-02 23:35:36 +0530

    Update DateTimeServlet.java

--- Day-6/src/main/java/com/nisum/DateTimeServlet.java
+++ Day-6/src/main/java/com/nisum/DateTimeServlet.java
@@ -1,23 +1,33 @@
 package com.nisum;
 
-import javax.servlet.*;
 import javax.servlet.http.*;
+import javax.servlet.ServletException;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class DateTimeServlet extends HttpServlet {
+public class CurrentTimeServlet extends HttpServlet {
+
+    private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("dd MMM yyyy, hh:mm a");
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        LocalDateTime now = LocalDateTime.now();
+        String formattedTime = getCurrentFormattedTime();
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().write(buildHtmlResponse(formattedTime));
+    }
 
-        resp.setContentType("text/html");
-        resp.getWriter().println("<h2>Current Date and Time:</h2>");
-        resp.getWriter().println("<p>" + now.format(formatter) + "</p>");
+    private String getCurrentFormattedTime() {
+        return LocalDateTime.now().format(TIME_FORMAT);
+    }
+
+    private String buildHtmlResponse(String time) {
+        return "<html><body>" +
+               "<h1>Server Time</h1>" +
+               "<p><em>" + time + "</em></p>" +
+               "</body></html>";
     }
 }
