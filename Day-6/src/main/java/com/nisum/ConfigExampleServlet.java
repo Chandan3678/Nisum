4d1879917c8503ce65ff5a80a338eb7ea85fccc3
Chandan3678 <rathchandan88@gmail.com>
2025-06-02 23:32:32 +0530

    Update ConfigExampleServlet.java

--- Day-6/src/main/java/com/nisum/ConfigExampleServlet.java
+++ Day-6/src/main/java/com/nisum/ConfigExampleServlet.java
@@ -1,8 +1,8 @@
 package com.nisum;
 
+import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.io.IOException;
 
 public class ConfigExampleServlet extends HttpServlet {
 
@@ -12,18 +12,26 @@ public class ConfigExampleServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        this.developerName = config.getInitParameter("developerName");
-        this.project = config.getInitParameter("project");
-        System.out.println("Servlet initialized with developerName: " + developerName + ", project: " + project);
+        developerName = config.getInitParameter("developerName");
+        project = config.getInitParameter("project");
+
+        System.out.printf("Servlet initialized with developerName: %s, project: %s%n",
+                          developerName, project);
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        resp.setContentType("text/html");
-        resp.getWriter().println("<h2>Initialization Parameters</h2>");
-        resp.getWriter().println("<p>Developer: " + developerName + "</p>");
-        resp.getWriter().println("<p>Project: " + project + "</p>");
+        response.setContentType("text/html");
+
+        StringBuilder htmlResponse = new StringBuilder();
+        htmlResponse.append("<html><body>")
+                    .append("<h2>Initialization Parameters</h2>")
+                    .append("<p>Developer: ").append(developerName).append("</p>")
+                    .append("<p>Project: ").append(project).append("</p>")
+                    .append("</body></html>");
+
+        response.getWriter().write(htmlResponse.toString());
     }
 }
