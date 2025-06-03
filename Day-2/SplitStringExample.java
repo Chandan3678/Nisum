09348e319747fa2813fe711f16998bf128e07403
Chandan3678 <rathchandan88@gmail.com>
2025-06-03 12:12:42 +0530

    Update SplitStringExample.java

--- Day-2/SplitStringExample.java
+++ Day-2/SplitStringExample.java
@@ -1,6 +1,6 @@
 		public class SplitStringExample {
    		 	public static void main(String[] args) {
-        				String data = "iam,Avinash,Rath";
+        				String data = "iam,Chandan,Rath";
         				String[] names = data.split(",");
         				for (String name : names) {
             			System.out.println(name);
