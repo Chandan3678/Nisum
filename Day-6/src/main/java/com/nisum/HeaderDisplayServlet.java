e7cf3ec34d867597d376da8122651639966b4d00
Chandan3678 <rathchandan88@gmail.com>
2025-06-03 11:52:00 +0530

    Update HeaderDisplayServlet.java

--- Day-6/src/main/java/com/nisum/HeaderDisplayServlet.java
+++ Day-6/src/main/java/com/nisum/HeaderDisplayServlet.java
@@ -1,24 +1,33 @@
-package com.nisum;
-import java.io.*;
+package com.nisum.web;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 
-public class HeaderDisplayServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
+public class RequestHeaderViewer extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+        
+        res.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = res.getWriter();
 
-        out.println("<html><body><h2>Request Headers</h2><ul>");
+        out.println("<!DOCTYPE html>");
+        out.println("<html><head><title>HTTP Headers</title></head><body>");
+        out.println("<h1>Incoming Request Headers</h1>");
+        out.println("<table border='1' cellpadding='5'><tr><th>Header Name</th><th>Header Value</th></tr>");
 
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String name = headerNames.nextElement();
-            String value = request.getHeader(name);
-            out.println("<li><strong>" + name + ":</strong> " + value + "</li>");
+        Enumeration<String> headers = req.getHeaderNames();
+        while (headers.hasMoreElements()) {
+            String headerName = headers.nextElement();
+            String headerValue = req.getHeader(headerName);
+            out.printf("<tr><td>%s</td><td>%s</td></tr>%n", headerName, headerValue);
         }
 
-        out.println("</ul></body></html>");
+        out.println("</table>");
+        out.println("</body></html>");
     }
 }
